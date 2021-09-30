0d811a36b65fed0395ddb03d9f5b4e27c67d310c
Benjamin Russell <benjaminrobertrussell@gmail.com>
2021-09-29 21:53:44 -0400

    added google map api. set 3 automatated map styles dependable on day. began setup of mapping out markers for google map.

--- client/src/components/Explore/Map.js
+++ client/src/components/Explore/Map.js
@@ -1,9 +1,14 @@
-import React, {useEffect, useContext} from "react";
+import React, {useEffect, useContext, useState} from "react";
 import { CaptureContext } from "../CaptureContext";
-import { GoogleMap , withScriptjs, withGoogleMap} from 'react-google-maps';
+import { GoogleMap , withScriptjs, withGoogleMap, Marker, InfoWindow} from 'react-google-maps';
+import Midnight from './Styles/Midnight';
+import Day from './Styles/Day';
+import Rise from './Styles/Rise';
 import GoogleMapReact from 'react-google-maps';
 import styled from "styled-components";
 import Loader from '../Common/Loader'
+import moment from 'moment';
+
 
 function thisMap() {
     const {
@@ -13,16 +18,63 @@ function thisMap() {
         myLocation,
         setMyLocation
     } = useContext(CaptureContext);
-    
-    console.log("pos:" , myLocation);
-    
+
+    const Time = moment().calendar()
+    let mapStyle;
+    if (Time.includes("6")) {
+        mapStyle = Rise;
+    }
+    if (Time.includes("P")) {
+        mapStyle = Midnight;
+    } else {
+        mapStyle = Day;
+    }
+
+    const [selectedMarker, setSelectedMarker] = useState(null);
+
     return(
         <>
             { (myLocation) ? (
                 <GoogleMap
                     defaultZoom={10}
                     defaultCenter={{ lat: myLocation.coords.latitude, lng: myLocation.coords.longitude}}
-                />
+                    defaultOptions={{ 
+                        styles: mapStyle,
+                        // streetViewControl: false,
+                        disableDefaultUI: true
+                    }}
+                    
+                >
+                    {/* { locaions.map(location => { */}
+                        <Marker 
+                            key={Math.floor(Math.random) * 99999999}
+                            position={{ lat: myLocation.coords.latitude, lng: myLocation.coords.longitude }}
+                            icon={{
+                                url: `/newUser.png`,
+                                scaledSize: new window.google.maps.Size(25, 25)
+                            }}
+                            onClick={() => {
+                                setSelectedMarker(location)
+                            }}
+                        />
+                    {/* })} */}
+
+                    { selectedMarker && (
+                        <InfoWindow
+                            position={{ lat: myLocation.coords.latitude, lng: myLocation.coords.longitude }}
+                            onCloseClick={()=>{
+                                setSelectedMarker(null);
+                            }}
+                        >
+                            <div>
+                                <div>animal</div>
+                                <div>by user</div>
+                                <div>time stamp</div>
+                                <div>image</div>
+                            </div>
+                        </InfoWindow>
+                    )}
+                </GoogleMap>
             ) : (
                 <Loader />
             )}
@@ -36,7 +88,7 @@ export default function Map() {
     return (
         <MapWrapper>
             <TheMap
-                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`}
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
                 loadingElement={<div style={{ height: "100%" }} />}
                 containerElement={<div style={{ height: "100%" }} />}
                 mapElement={<div style={{ height: "100%" }} />}
---
+++ client/src/components/Explore/Styles/Day.js
@@ -0,0 +1,173 @@
+export default [
+    {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#7c93a3"
+            },
+            {
+                "saturation": "0"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.country",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#c2d1d6"
+            },
+            {
+                "saturation": "0"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#d9e3e5"
+            },
+            {
+                "saturation": "0"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#e1ebeb"
+            },
+            {
+                "saturation": "0"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "saturation": "0"
+            },
+            {
+                "color": "#eef4f4"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#98e691"
+            },
+            {
+                "saturation": "0"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "saturation": "0"
+            },
+            {
+                "color": "#f4fafa"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#c2d1d6"
+            },
+            {
+                "saturation": "0"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#a9b4b8"
+            },
+            {
+                "saturation": "0"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#eef4f4"
+            },
+            {
+                "saturation": "0"
+            }
+        ]
+    },
+    {
+        "featureType": "transit.line",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#c2d1d6"
+            },
+            {
+                "saturation": "0"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#66e1f0"
+            },
+            {
+                "saturation": "0"
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    }
+]
